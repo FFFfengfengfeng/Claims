902852b19b81efb3cda89ffab7600e3b339395e0
FFFfengfengfeng <1098865109@qq.com>
2018-05-07 16:46:16 +0800

    note

---
+++ test-io.js
@@ -0,0 +1,18 @@
+// console.log('Hello');
+
+// setTimeout(() => {
+//     console.log('World');
+// }, 1000);
+
+let start = Date.now();
+
+console.log(start);
+
+setTimeout(() => {
+    console.log(Date.now() - start);
+    for (let i = 0; i < 1000000000; i ++) {}
+}, 1000);
+
+setTimeout(() => {
+    console.log(Date.now() - start);
+}, 2000);
