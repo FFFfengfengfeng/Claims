6ed03e5f49983645384768deb0bd41e7bb5b8fe7
FFFfengfengfeng <1098865109@qq.com>
2018-05-10 17:01:35 +0800

    http API

---
+++ https/index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs   = require('fs');
+
+http.createServer((req, res) => {
+    res.writeHead('200', {
+        'Content-Type': 'image/jpg'
+    });
+    
+    var stream = fs.createReadStream('./26212855.jpg');
+    
+    stream.on('data', (data) => {
+        res.write(data);
+    });
+
+    stream.on('end', () => {
+        res.end();
+    });
+}).listen(7005);
