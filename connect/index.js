ef721d4ee5f5b2cc3c6eb71938cbdf7f55e47d78
FFFfengfengfeng <1098865109@qq.com>
2018-05-18 15:06:34 +0800

    connect中间件

--- connect/index.js
+++ connect/index.js
@@ -2,7 +2,19 @@ const connect = require('connect');
 const server = connect.createServer();
 
 // 记录日志
+server.use((req, res, next) => {
+    console.error('&s &s', req.method, req.url);
+});
 
-server.use(connect.static(__dirname + '/website'));
+server.use((req, res, next) => {
+    if (req.method === 'GET' && req.url.substr(0, 7) === '/images') {
 
-server.listen(7005);
+    } else {
+        next();
+    }
+});
+
+server.use((req, res, next) => {
+    res.writeHead(404);
+    res.end('Not found');
+});
