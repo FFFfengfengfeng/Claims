fab0f50bf3b4b6bf00d8c9a72dfa7e680cd1e8c7
FFFfengfengfeng <1098865109@qq.com>
2018-05-22 11:43:24 +0800

    express获取segmen搜索结果

--- express-demo/server.js
+++ express-demo/server.js
@@ -1,4 +1,28 @@
 const express = require('express');
-
+const search = require('./search');
 const app = express.createServer();
 
+// ejs 配置
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.set('view options', {layout: false});
+
+// 路由
+// 首页
+app.get('/', function(req, res) {
+    res.render('index');
+});
+// 搜索结果页
+app.get('/search', function(req, res, next) {
+    search(req.query.q, function(err, segmen) {
+        if (err) {
+            return next(err);
+        }
+        res.render('search', {
+            results: segmen,
+            search: req.query.q
+        });
+    });
+});
+
+app.listen(7005);
